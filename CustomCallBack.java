291c994ebdbb73a13dba1db56f21cded230fedc1
Țurcan Andrian <andrian.t@ideaction.io>
2018-12-11 12:45:00 +0200

    Add files via upload

---
+++ CustomCallBack.java
@@ -0,0 +1,53 @@
+package io.ideaction.slipzz.APIClasses;
+
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.WindowManager;
+import android.widget.Toast;
+
+import org.json.JSONObject;
+
+import io.ideaction.slipzz.LoadingDialog;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class CustomCallBack<T> implements Callback<T> {
+
+    LoadingDialog mLoadingDialog;
+    Context context;
+
+    public CustomCallBack(Context context) {
+        this.context = context;
+        mLoadingDialog = new LoadingDialog();
+        mLoadingDialog.show(((AppCompatActivity)context).getSupportFragmentManager() , "");
+        ((AppCompatActivity) context).getWindow().setFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE,
+                WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE);
+    }
+
+    @Override
+    public void onResponse(Call<T> call, Response<T> response) {
+
+        mLoadingDialog.dismiss();
+        ((AppCompatActivity) context).getWindow().clearFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE);
+        if (!response.isSuccessful()) {
+            try {
+                JSONObject jObjError = new JSONObject(response.errorBody().string());
+                Toast.makeText(context, jObjError.getString("message"), Toast.LENGTH_LONG).show();
+            } catch (Exception e) {
+                Toast.makeText(context, e.getMessage(), Toast.LENGTH_LONG).show();
+            }
+        }
+
+    }
+
+    @Override
+    public void onFailure(Call<T> call, Throwable t) {
+        mLoadingDialog.dismiss();
+        ((AppCompatActivity) context).getWindow().clearFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE);
+        Toast.makeText(context , "We are unable to connect you. Please try again or check network settings" , Toast.LENGTH_LONG).show();
+        Log.d("retrofitError" , t.getMessage());
+    }
+}
---
+++ LoadingDialog.java
@@ -0,0 +1,28 @@
+package io.ideaction.slipzz;
+
+import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+public class LoadingDialog extends DialogFragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.dialog_loading, container, false);
+        ProgressBar loading_indicator = v.findViewById(R.id.loading_indicator);
+        loading_indicator.getIndeterminateDrawable()
+                .setColorFilter(Color.WHITE, PorterDuff.Mode.SRC_IN);
+        return v;
+    }
+
+
+
+}
